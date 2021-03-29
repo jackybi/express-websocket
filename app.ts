45f39665f1b121742ff2beaf2b36d9c8075d0efe
iRace-it <51731371+iRace-it@users.noreply.github.com>
2021-03-29 21:15:37 +0800

    init

---
+++ app.ts
@@ -0,0 +1,28 @@
+import * as express from "express";
+import * as http from "http";
+import * as WebSocket from "ws";
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws: WebSocket) => {
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message: string) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Hi there, I am a WebSocket server');
+});
+
+//start our server
+server.listen(process.env.PORT || 8999, () => {
+    console.log(`Server started on port ${server.address().port} :)`);
+});
+
+module.exports = app;
